47ff915f4a5e70ebc19101635b1f66dc0bda2345
your-username <sanderjohn942@gmail.com>
2024-07-21 11:25:54 -0400

    Update for deployment

--- src/main/java/com/example/StockPickerController/StockPickerController.java
+++ src/main/java/com/example/StockPickerController/StockPickerController.java
@@ -22,7 +22,7 @@ public class StockPickerController {
     @Autowired
     private StockScalperFetch stockScalperFetch;
 
-    @CrossOrigin(origins = {"http://localhost:3000", "https://669ca762ec4953971cb32f82--magnificent-marshmallow-d2bdb6.netlify.app"})
+    @CrossOrigin(origins = {"https://669ca762ec4953971cb32f82--magnificent-marshmallow-d2bdb6.netlify.app","http://localhost:3000"})
     @GetMapping("/live")
     public List<StockData> getLiveStockData(
             @RequestParam String symbols,
